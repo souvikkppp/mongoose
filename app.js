2a3d3ed11a80233a36aae45e8068783364f85fd9
soovik kundu <sovikkunducom19@gmail.com>
2022-08-17 21:32:05 +0530

    challenge modified

--- app.js
+++ app.js
@@ -3,28 +3,30 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
 
 
-//this is the foundation of every document in our database
-//ex: every fruit data will store in this format
+// this is the foundation of every document in our database
+// ex: every fruit data will store in this format
 
-//// const fruitSchema = new mongoose.Schema ({
-////   name: String,
-////   rating: Number,
-////   review: String
-//// });/
-//
-//in this frist parameter we have to give a singular string that mongoose will convert
-//in plural so here its Fruit it will be fruits in our fruitsDB database,  then fruitSchema
-//parameter will  define the structure of our collection called fruits
+const fruitSchema = new mongoose.Schema ({
+  name: String,
+  rating: Number,
+  review: String
+});/
 
-//// const Fruit = mongoose.model("Fruit", fruitSchema);
-//
-//// const fruit = new Fruit ({
-////  name: "apple",
-////  rating: 4,
-////  review: "it's teast so good"
-//// });
-//
-// fruit.save();
+// in this frist parameter we have to give a singular string that mongoose will convert
+// in plural so here its Fruit it will be fruits in our fruitsDB database,  then fruitSchema
+// parameter will  define the structure of our collection called fruits
+
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const fruit = new Fruit ({
+ name: "apple",
+ rating: 4,
+ review: "it's teast so good"
+});
+
+fruit.save();
+
+//challenge create a new collection called people and add data
 
 const peopleSchema = new mongoose.Schema({
  name: String,
