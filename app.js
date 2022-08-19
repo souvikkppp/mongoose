4cba415ab96d1f5bf3e323664a0ee2505f06149a
soovik kundu <sovikkunducom19@gmail.com>
2022-08-19 11:24:10 +0530

    validation

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const mongoose = require("mongoose");
 //this fruitsDB is the database name that we want create
-mongoose.connect("mongodb://localhost:27017/fruitsDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true
+});
 
 
 // this is the foundation of every document in our database
 // ex: every fruit data will store in this format
 
-const fruitSchema = new mongoose.Schema ({
-  name: String,
-  rating: Number,
-  review: String
-});/
+const fruitSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "please your name it can't be null!"]
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  review: {
+    type: String,
+    required: true
+  }
+});
 
 // in this frist parameter we have to give a singular string that mongoose will convert
 // in plural so here its Fruit it will be fruits in our fruitsDB database,  then fruitSchema
@@ -18,19 +30,63 @@ const fruitSchema = new mongoose.Schema ({
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const fruit = new Fruit ({
- name: "apple",
- rating: 4,
- review: "it's teast so good"
+const fruit = new Fruit({
+  name: "peach",
+  rating: 6,
+  review: "it's teast so good"
 });
 
-fruit.save();
+// fruit.save();
+
+const kiwi = new Fruit({
+  name: "kiwi",
+  rating: 5,
+  review: "nice"
+});
+
+const banana = new Fruit({
+  name: "banana",
+  rating: 4,
+  review: "good"
+});
+
+const mango = new Fruit({
+  name: "mango",
+  rating: 4,
+  review: "bad"
+});
+
+// Fruit.insertMany([kiwi, banana, mango], (err) => {
+//   console.log(err)
+// });
+
+
+
+
+Fruit.find((err, fruits) => {
+  if (err) {
+  console.log(err);
+} else {
+  //to auto close our mongoose connect
+  mongoose.connection.close();
+
+  fruits.forEach((fruit) => {
+    console.log(fruit.name);
+  });
+ }
+
+});
+
+
+
+
+
 
 //challenge create a new collection called people and add data
 
 const peopleSchema = new mongoose.Schema({
- name: String,
- age: Number
+  name: String,
+  age: Number
 });
 
 const People = mongoose.model("People", peopleSchema);
@@ -40,4 +96,4 @@ const people = new People({
   age: 37
 })
 
-people.save();
+// people.save();
